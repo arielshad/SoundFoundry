95e1a05dd3ef833afba6ad98a20626791732cabc
fruitsaladisland <andrewkchan.akc@gmail.com>
2016-08-06 00:32:22 +0000

    scaffolding for separate client code

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+
+if(document.querySelectorAll("a").length){
+    require.ensure([], () => {
+        const Button = require("./Components/Button").default;
+        const button = new Button("google.com");
+        
+        button.render("a");
+    })
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./src",
+    output: {
+        path: "builds/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loader: "babel",
+                include: __dirname + "/src"
+            },
+            {
+                test: /\.scss/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.html/,
+                loader: "html"
+            }
+        ]
+    }
+};
+
